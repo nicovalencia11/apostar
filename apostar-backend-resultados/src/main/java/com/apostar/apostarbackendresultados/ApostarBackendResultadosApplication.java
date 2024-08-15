ffd86b9fc48ef67c6f1c667e0cf67273e8590fbf
Nicolas Valencia <nicolasvalenciamadrid@gmail.com>
2024-08-15 16:42:13 -0500

    Creacion de estructura y bases para el front y el back, se configura los docker correspondientes y el docker compose

---
+++ apostar-backend-resultados/src/main/java/com/apostar/apostarbackendresultados/ApostarBackendResultadosApplication.java
@@ -0,0 +1,13 @@
+package com.apostar.apostarbackendresultados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApostarBackendResultadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApostarBackendResultadosApplication.class, args);
+    }
+
+}
